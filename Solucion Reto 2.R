5f5f7db553880b41150037c30140345f0985cb2c
J Emmanuel G C <emmanuel_12_gc@hotmail.com>
2021-01-28 03:12:53 +0000

    Correccion Reto 2

--- Solucion Reto 2.R
+++ Solucion Reto 2.R
@@ -2,13 +2,8 @@ library(rvest)
 
 theurl <- "https://www.glassdoor.com.mx/Sueldos/data-scientist-sueldo-SRCH_KO0,14.htm"
 file <- read_html(theurl)
-# Selecciona pedazos dentro del HTML para identificar la tabla
 
 tables <- html_nodes(file, "table")  
-# Hay que analizar 'tables' para determinar cual es la posiciÃ³n en la lista 
-# que contiene la tabla, en este caso es la no. 4
-
-# Extraemos la tabla de acuerdo a la posiciÃ³n en la lista
 
 table1 <- html_table(tables[1], fill = TRUE)
 
